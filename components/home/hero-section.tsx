243bccb09656febf78a579929405b24c112654f4
nicoloprog <nicosubywrx@gmail.com>
2026-04-21 16:19:23 -0400

    chanig dot

--- components/home/hero-section.tsx
+++ components/home/hero-section.tsx
@@ -300,7 +300,7 @@ export function HeroSection() {
           className="absolute inset-0 min-h-full"
           style={{
             backgroundImage:
-              "radial-gradient(circle, rgba(4, 108, 254, 0.19) 1px, transparent 2px)",
+              "radial-gradient(circle, rgba(4, 108, 254, 0.19) 1px, transparent 1px)",
             backgroundSize: "19px 19px",
             maskImage:
               "radial-gradient(ellipse 80% 70% at 50% 40%, black 40%, transparent 100%)",
@@ -311,7 +311,7 @@ export function HeroSection() {
       {/* Blue glow */}
       <div
         aria-hidden
-        className="absolute top-[42%] sm:top-[55%]  left-1/2 -translate-x-1/2 w-[1800px] h-[240px] rounded-[50%] pointer-events-none"
+        className="absolute top-[40%] sm:top-[55%]  left-1/2 -translate-x-1/2 w-[1800px] h-[240px] rounded-[50%] pointer-events-none"
         style={{
           background:
             "radial-gradient(ellipse, rgba(52, 148, 227, 0.34) 0%,  transparent 70%)",
